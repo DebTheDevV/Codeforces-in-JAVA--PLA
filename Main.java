9b96c259261c5225fbf86c36653f7dc75ead67ed
DebTheDevV <debthedevv@gmail.com>
2026-07-27 22:21:44 +0530

    day 8-236A

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        if (scanner.hasNext()) {
+            String username = scanner.next();
+            
+            
+            HashSet<Character> distinctChars = new HashSet<>();
+            for (int i = 0; i < username.length(); i++) {
+                distinctChars.add(username.charAt(i));
+            }
+            
+            
+            if (distinctChars.size() % 2 == 0) {
+                System.out.println("CHAT WITH HER!");
+            } else {
+                System.out.println("IGNORE HIM!");
+            }
+        }
+        
+        scanner.close();
+    }
+}
